18408bb66a6b9f67d62ee42ec375738a41bc671a
O-sawa <suppon-turtle622@ezweb.ne.jp>
2021-01-07 10:15:30 +0900

    initialCommit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float x;
+        x = Input.GetAxis("Horizontal");
+        float z;
+        z = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(x, 0, z) * speed * Time.deltaTime);
+    }
+}
