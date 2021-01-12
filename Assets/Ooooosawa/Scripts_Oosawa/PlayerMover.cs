d90fe72135c3a44af2f2de52520f9e8acef4eb78
O-sawa <suppon-turtle622@ezweb.ne.jp>
2021-01-12 14:57:13 +0900

    Playerの基本移動・アニメーション実装

---
+++ Assets/Ooooosawa/Scripts_Oosawa/PlayerMover.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField]
+    float moveSpeed = 5f;
+    Vector3 moveInput;
+
+    Vector3 pos;
+
+    Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+
+        pos = transform.position;
+    }
+    void FixedUpdate()
+    {
+
+        Move();
+    }
+
+    void Move()
+    {
+        var x = Input.GetAxisRaw("Horizontal");
+        var z = Input.GetAxisRaw("Vertical");
+
+        moveInput = new Vector3(x, 0, z).normalized;
+
+        rb.velocity = moveInput * moveSpeed;
+
+        Rotate();
+        Animation();
+
+    }
+
+    void Rotate()
+    {
+        var curPos = transform.position;
+
+        var diff = curPos - pos;
+
+        if (diff.magnitude > 0.01f)
+        {
+            rb.rotation = Quaternion.LookRotation(diff);
+        }
+
+        pos = transform.position;
+    }
+
+    void Animation()
+    {
+        if (rb.velocity.magnitude > 0 && !animator.GetBool("isRunning"))
+            animator.SetBool("isRunning", true);
+        else if (rb.velocity.magnitude <= 0 && animator.GetBool("isRunning"))
+            animator.SetBool("isRunning", false);
+    }
+}
