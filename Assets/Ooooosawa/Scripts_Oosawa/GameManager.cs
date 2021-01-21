58e45ebf9407222d9137b27c1320b08d8d21ee50
so-mania <suppon622@yahoo.co.jp>
2021-01-21 11:01:12 +0900

    GameManager,TitleScene

--- Assets/Ooooosawa/Scripts_Oosawa/GameManager.cs
+++ Assets/Ooooosawa/Scripts_Oosawa/GameManager.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCore : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int RedCount { get; set; }
+
+    public int BlueCount { get; set; }
+
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
