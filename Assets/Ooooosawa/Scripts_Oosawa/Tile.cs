e4679b670b4c39baba92eb23bb2c122802c9dc1d
so-mania <suppon622@yahoo.co.jp>
2021-01-27 11:33:16 +0900

    タイルの色の同期完了 ほぼ完成

--- Assets/Ooooosawa/Scripts_Oosawa/Tile.cs
+++ Assets/Ooooosawa/Scripts_Oosawa/Tile.cs
@@ -3,24 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class Tile : MonoBehaviour
+public class Tile : MonoBehaviourPunCallbacks
 {
+    PunTest pTest;
     Color myColor;
 
     private void Awake()
     {
+        ColorSerializer.Register();
+        pTest = GameObject.Find("PUNPUNPUN").GetComponent<PunTest>();
         myColor = GetComponent<Renderer>().material.color;
     }
 
     private void OnTriggerEnter(Collider col)
     {
         var color = col.GetComponent<PlayerCore>().myColor;
-        if (this.myColor != color)
+        if (this.myColor != color && pTest.ServerFlg)
         {
+            photonView.RPC("ChangeMyColor", RpcTarget.All, color);
             ChangeMyColor(color);
         }
     }
 
+    [PunRPC]
     void ChangeMyColor(Color color)
     {
         GetComponent<Renderer>().material.color = color;
